b00e828d01921f685c7b997e71f2d1f07811c5ff
greenina <igreen0485@gmail.com>
2020-12-31 00:52:38 +0900

    tab structure

---
+++ app/src/main/java/com/example/viewpager2/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.viewpager2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.example.viewpager2.*
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+
+class MainActivity : FragmentActivity() {
+    private lateinit var viewPager: ViewPager2
+    private lateinit var tabLayout: TabLayout
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        viewPager = findViewById(R.id.viewPager)
+        tabLayout = findViewById(R.id.tabLayout)
+        val adapter = FragmentAdapter(this)
+        val fragments = listOf<Fragment>(HomeFragment(), OneFragment(),TwoFragment(),ThreeFragment())
+        val tabTitles = listOf<String>("Home","Tab1","Tab2", "Tab3")
+        adapter.fragments.addAll(fragments)
+        viewPager.adapter = adapter
+        TabLayoutMediator(tabLayout, viewPager){tab, position ->
+            tab.text = tabTitles[position]
+        }.attach()
+    }
+}
+
+class FragmentAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity){
+    var fragments = mutableListOf<Fragment>()
+    override fun getItemCount(): Int {
+        return fragments.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return fragments[position]
+    }
+
+}
