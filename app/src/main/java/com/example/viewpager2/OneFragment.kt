7067f4afb504e7c193f328ab905c4ea5a9910b30
greenina <igreen0485@gmail.com>
2021-01-01 21:14:36 +0900

    recycle null error solved

--- app/src/main/java/com/example/viewpager2/OneFragment.kt
+++ app/src/main/java/com/example/viewpager2/OneFragment.kt
@@ -55,8 +55,7 @@ class OneFragment : Fragment() {
         adapter = PhoneAdapter(list)
         recycler?.adapter = adapter
         recycler?.layoutManager = LinearLayoutManager(getContext())
-
-    }
+        }
 
     fun setSearchListener() {
         editSearch.addTextChangedListener(object: TextWatcher {
@@ -91,40 +90,30 @@ class OneFragment : Fragment() {
 
     fun getPhoneNumbers(sort:String, searchName:String?) : List<Phone> {
         val list = mutableListOf<Phone>()
-        // 1. 전화번호 Uri
         val phoneUri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI
-        // 2.1 전화번호에서 가져올 컬럼 정의
         val projections = arrayOf(
             ContactsContract.CommonDataKinds.Phone.CONTACT_ID
             , ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME
             , ContactsContract.CommonDataKinds.Phone.NUMBER)
         var wheneClause:String? = null
         var whereValues:Array<String>? = null
-        // searchName에 값이 있을 때만 검색을 사용한다
         if(searchName?.isNotEmpty() ?: false) {
             wheneClause = ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " like ?"
             whereValues = arrayOf("%$searchName%")
         }
-        // 2.3 정렬쿼리 사용
         val optionSort = ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " $sort"
-        // 3. 테이블에서 주소록 데이터 쿼리
         val cursor = getActivity()?.contentResolver?.query(phoneUri, projections, wheneClause, whereValues, optionSort)
 
-        // 4. 반복문으로 아이디와 이름을 가져오면서 전화번호 조회 쿼리를 한번 더 돌린다.
         while(cursor?.moveToNext()?:false) {
             val id = cursor?.getString(0)
             val name = cursor?.getString(1)
             var number = cursor?.getString(2)
-            // 개별 전화번호 데이터 생성
             val phone = Phone(id, name, number)
-            // 결과목록에 더하기
             list.add(phone)
         }
-        // 결과목록 반환
         return list
     }
-
-    // 권한처리 코드
+    
     fun checkAndStart() {
         if( isLower23() || isPermitted()) {
             startProcess()
