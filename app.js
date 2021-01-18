7ec686146c5a7881bbd25ccdd77c413b75542ced
BBang3 <hwsmin920@gmail.com>
2021-01-18 11:21:12 +0000

    User register/login

--- app.js
+++ app.js
@@ -1,41 +1,63 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
-var app = express();
+const app = express();
+const PORT = 8080;
+const cors = require("cors");
+
+const mongoose = require("mongoose");
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+mongoose.connect(
+  `mongodb://192.249.18.247:27017/madcamp_week3`,
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  },
+  () => {
+    console.log("connected to DB");
+  }
+);
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
+});
+
+app.listen(PORT, function () {
+  console.log(`Express server listening on port ${PORT}`);
 });
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,87 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+const { auth } = require("../middleware/auth");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("User router");
 });
+
+router.post("/register", async (req, res) => {
+  //회원 가입 할때 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터 베이스에 넣어준다.
+  try {
+    const user = new User(req.body);
+    const output = await user.save();
+
+    console.log("Register success");
+    res.status(200).json({ success: true });
+  } catch (error) {
+    res.status(500).json({ success: false, ...error });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  //요청된 이메일을 데이터베이스에서 있는지 찾는다.
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(404).json({
+        loginSuccess: false,
+        message: "User not found",
+      });
+    }
+
+    // 요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인.
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.status(500).json({
+          loginSuccess: false,
+          message: "Wrong password",
+        });
+      //비밀번호까지 맞다면 토큰을 생성하기.
+      user.generateToken((err, user) => {
+        if (err) return res.status(500).send(err);
+
+        // 토큰을 저장한다. 어디에? 쿠키,로컬 스토리지
+        return res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id }); //리턴 값이 lofinSuccess true와 userid 임
+      });
+    });
+  } catch (error) {
+    res.status(500).json({
+      loginSuccess: false,
+      message: error,
+    });
+  }
+});
+
+router.get("/auth", auth, (req, res) => {
+  //여기까지 미들웨어를 통과해왔다는 얘기는 Authecation 이 true 라는 말
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
+  });
+});
+
+// router.get("/logout", auth, (req, res) => {
+//   console.log("여기에 들어왔나?? ㅜㅜ");
+//   User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
+//     console.log("여기에 들어왔나?? ㅜㅜ 2");
+//     if (err) return res.json({ success: falsedfgdfg, err });
+//     return res.status(200).send({
+//       success: true,
+//     });
+//   });
+// });
 
 module.exports = router;
