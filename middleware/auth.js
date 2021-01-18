da9397d45718b68d3cfe0d7c3d1d008f8459af2a
BBang3 <hwsmin920@gmail.com>
2021-01-18 20:34:16 +0000

    Logout modified

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,8 +4,8 @@ let auth = (req, res, next) => {
   //인증 처리를 하는 곳
 
   //클라이언트 쿠키에서 토큰을 가져온다.
-  console.log(req.cookies);
-  let token = req.cookies.x_auth;
+  console.log("cooke:", req.cookies);
+  let token = req.cookies.token;
 
   console.log("auth token: " + token);
   //토큰을 복호화 한 후 유저를 찾는다.
@@ -14,7 +14,7 @@ let auth = (req, res, next) => {
     if (err) throw err;
     console.log("middleware에 존재 ");
     if (!user) {
-      return res.json({ isAuth: false, error: true });
+      return res.json({ isAuth: false, error: true, success: false });
     }
     req.token = token;
     req.user = user;
--- routes/users.js
+++ routes/users.js
@@ -11,6 +11,7 @@ router.get("/", function (req, res, next) {
 router.post("/register", async (req, res) => {
   //회원 가입 할때 필요한 정보들을 client에서 가져오면
   //그것들을 데이터 베이스에 넣어준다.
+  console.log(req.body);
   try {
     const user = new User(req.body);
     const output = await user.save();
@@ -23,6 +24,7 @@ router.post("/register", async (req, res) => {
 });
 
 router.post("/login", async (req, res) => {
+  console.log(req.body);
   //요청된 이메일을 데이터베이스에서 있는지 찾는다.
   try {
     const user = await User.findOne({ email: req.body.email });
@@ -43,12 +45,13 @@ router.post("/login", async (req, res) => {
       //비밀번호까지 맞다면 토큰을 생성하기.
       user.generateToken((err, user) => {
         if (err) return res.status(500).send(err);
-
         // 토큰을 저장한다. 어디에? 쿠키,로컬 스토리지
-        return res
-          .cookie("x_auth", user.token)
-          .status(200)
-          .json({ loginSuccess: true, userId: user._id }); //리턴 값이 lofinSuccess true와 userid 임
+
+        return res.json({
+          loginSuccess: true,
+          userId: user._id,
+          token: user.token,
+        }); //리턴 값이 lofinSuccess true와 userid 임
       });
     });
   } catch (error) {
@@ -73,15 +76,16 @@ router.get("/auth", auth, (req, res) => {
   });
 });
 
-// router.get("/logout", auth, (req, res) => {
-//   console.log("여기에 들어왔나?? ㅜㅜ");
-//   User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
-//     console.log("여기에 들어왔나?? ㅜㅜ 2");
-//     if (err) return res.json({ success: falsedfgdfg, err });
-//     return res.status(200).send({
-//       success: true,
-//     });
-//   });
-// });
+router.get("/logout/:user_id", auth, async (req, res) => {
+  console.log("params: " + req.params);
+  User.findOneAndUpdate(
+    { _id: req.params.user_id },
+    { token: "" },
+    (err, user) => {
+      if (err) return res.status(500).json({ success: false, err });
+      return res.status(200).json({ success: true });
+    }
+  );
+});
 
 module.exports = router;
