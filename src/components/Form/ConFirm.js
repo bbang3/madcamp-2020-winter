b41539ac15b7e6c0d9e96c82cb7c82da5b069d36
BBang3 <hwsmin920@gmail.com>
2021-01-18 14:49:07 +0900

    Bug fixed

--- src/components/Form/ConFirm.js
+++ src/components/Form/ConFirm.js
@@ -1,10 +1,7 @@
 import React from "react";
 
-const ConFirm = ({values}]) => {
-  return <div>
-      주어진 정보가 맞는지 다시 한번 확인해주세요.
-
-  </div>;
+const ConFirm = ({ values }) => {
+  return <div>주어진 정보가 맞는지 다시 한번 확인해주세요.</div>;
 };
 
 export default ConFirm;
